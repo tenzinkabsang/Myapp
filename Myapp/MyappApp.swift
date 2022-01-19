0ea2e9f446350f0f123145ae6bcd4e3fe717c3d8
Tenzin Kabsang <tenzinkabsang@gmail.com>
2022-01-19 11:22:02 -0700

    Initial Commit

---
+++ Myapp/MyappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyappApp.swift
+//  Myapp
+//
+//  Created by Tenzin Kabsang on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct MyappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
